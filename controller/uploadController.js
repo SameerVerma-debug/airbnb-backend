d8c23c79feea52a8ea3cf8d97dedb025be3749d8
SameerVerma-debug <sameerv314@gmail.com>
2024-04-21 02:12:22 +0530

    null-checks

--- controller/uploadController.js
+++ controller/uploadController.js
@@ -5,7 +5,7 @@ const basePath = path.join(__dirname,"..","uploads/")
 
 const handleUpload = async (req, res) => {
   const uploadedFiles = [];
-  for (let i = 0; i < req.files.length; i++) {
+  for (let i = 0; i < req.files?.length; i++) {
     const { filename, originalname } = req.files[i];
     const parts = originalname.split(".");
     const ext = parts[parts.length - 1];
