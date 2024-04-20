941a75c21968f0499409b950dae97a0882e08dc7
SameerVerma-debug <sameerv314@gmail.com>
2024-04-21 01:54:14 +0530

    client-url-update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connectDB = require("./config/connectdb");
 const path = require("path");
 const cron = require("node-cron");
 const clearOldBookings = require("./utils/clearOldBookings");
-const CLIENT_URL = "http://localhost:5173";
+const CLIENT_URL = "https://visionary-squirrel-9db3c8.netlify.app";
 require("dotenv").config();
 connectDB();
 
